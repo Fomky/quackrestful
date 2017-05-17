88df5aab7e342495be90756874ec9af01d069647
fomky <fomyk@foxmail.com>
2017-05-17 11:08:38 +0800

    inital project

--- src/main/java/org/fomky/ratpack/core/utils/ChainUtil.java
+++ src/main/java/org/fomky/ratpack/core/utils/ChainUtil.java
@@ -12,14 +12,17 @@ import static ratpack.jackson.Jackson.json;
 public class ChainUtil {
 
     private static Chain addErrorSupport(Chain chain) throws Exception {
-        return chain.register(registry -> registry.add(ServerErrorHandler.class, (context, throwable) -> {
-            if(throwable.getMessage().contains("SessionKey[name='USERID'")){
-                //未登录返回
-                context.render(json(new Res(100, "提示用户登录！")));
-            }else{
-                context.render(json(new Res(500, throwable.getMessage())));
-            }
-        }));
+        return chain
+                .register(registry -> registry
+                        .add(ServerErrorHandler.class, (context, throwable) -> {
+                            if (throwable.getMessage().contains("SessionKey[name='USERID'")) {
+                                //未登录返回
+                                context.render(json(new Res(100, "提示用户登录！")));
+                            } else {
+                                context.render(json(new Res(500, throwable.getMessage())));
+                            }
+                        })
+                );
     }
 
     public static Chain addAllSupport(Chain chain) throws Exception {
