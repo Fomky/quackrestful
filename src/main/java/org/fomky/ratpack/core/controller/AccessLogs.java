59e9479150e7629080e3abd190ed7797756bb3ad
fomky <fomky@foxmail.com>
2017-06-30 14:39:15 +0800

    Change config of Redis HostAndPort, from 'session.redis.*' to 'spring.redis'

--- src/main/java/org/fomky/ratpack/core/controller/AccessLogs.java
+++ src/main/java/org/fomky/ratpack/core/controller/AccessLogs.java
@@ -1,14 +1,39 @@
 package org.fomky.ratpack.core.controller;
 
+import org.fomky.ratpack.core.entity.Res;
+import ratpack.error.ClientErrorHandler;
+import ratpack.error.ServerErrorHandler;
+import ratpack.error.internal.ErrorHandler;
 import ratpack.handling.Chain;
 import ratpack.handling.RequestLogger;
 import ratpack.handling.RequestOutcome;
 
+import static ratpack.jackson.Jackson.json;
+
 public abstract class AccessLogs implements CoreHandle {
     @Override
     public void execute(Chain chain) throws Exception {
-        chain.all(RequestLogger.of(this::access));
+        chain
+                .all(RequestLogger.of(this::access))
+                .register(registry -> registry
+                        .add(ServerErrorHandler.class,error())
+                        .add(ClientErrorHandler.class,client())
+                );
     }
 
-    abstract void access(RequestOutcome outcome);
+    public abstract void access(RequestOutcome outcome);
+
+    public ServerErrorHandler error(){
+        return  (context, throwable) -> {
+            if (throwable.getMessage().contains("SessionKey[name='USERID'")) {
+                //未登录返回
+                context.render(json(new Res(100, "提示用户登录！")));
+            } else {
+                context.render(json(new Res(500, throwable.getMessage())));
+            }
+        };
+    }
+    public ClientErrorHandler client(){
+        return (context, i) -> context.render(json(i));
+    }
 }
